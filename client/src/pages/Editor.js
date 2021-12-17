23ad1cac4e1a549d47401d516a4b565d651d19d0
domdelorenzo <delorenzo.breed@gmail.com>
2021-12-17 11:41:44 -0600

    Initialized editor playground

--- client/src/pages/Editor.js
+++ client/src/pages/Editor.js
@@ -1,34 +1,15 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import SidePane from '../components/SidePane';
-// import Content from '../components/Content';
-import React, { useMemo, useState } from 'react';
-import { createEditor } from 'slate';
-import { Slate, Editable, withReact } from 'slate-react';
-import DocPanel from '../components/DocPanel';
+import Content from '../components/Content';
 
 export default function Editor() {
-  const editor = useMemo(() => withReact(createEditor()), []);
-  const [value, setValue] = useState([
-    {
-      type: 'paragraph',
-      children: [{ text: 'Write, endlessly...' }]
-    }
-  ]);
   return (
     <div className="editor-container">
       <div className="side-pane-container">
         <SidePane />
       </div>
       <div className="content-container">
-        {/* <Content /> */}
-        <DocPanel />
-        {/* <Slate
-          editor={editor}
-          value={value}
-          onChange={(newValue) => setValue(newValue)}
-        >
-          <Editable className="slateBox" />
-        </Slate> */}
+        <Content />
       </div>
     </div>
   );
