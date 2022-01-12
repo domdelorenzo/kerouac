354c1ea171bbdc712f58fe7f872c42a3e419dd10
domdelorenzo <delorenzo.breed@gmail.com>
2022-01-11 20:22:38 -0600

    Added base_url import to document card

--- client/src/components/DocumentCard.jsx
+++ client/src/components/DocumentCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios';
+import { BASE_URL } from '../globals';
 
 const DocumentCard = (props) => {
   const [documentState, setDocumentState] = props.functions
